f53c1f2bb22f02a22ac4da2776b8182ef59a3d93
scherzocrk <renan.engmec@gmail.com>
2021-06-12 17:14:15 -0300

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ["css/stylesheet.css"]
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
